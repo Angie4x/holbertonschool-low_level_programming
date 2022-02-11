65cc7da561072a4f15d3e6abb5d0569f5295006e
Angie4x <angiel.aldanac@outlook.com>
2022-02-10 18:19:24 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function principa
+ *l
+ * Return: sucessfull 0.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
