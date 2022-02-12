580a7307b5590d1a4eb9cf047c40e4ee5c0bf696
Angie4x <angiel.aldanac@outlook.com>
2022-02-11 17:51:57 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
 /**
- * main - main function
- * Return: always 0.
+* main - print numbers
+* Return: Always 0
 */
 int main(void)
 {
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-	putchar(low);
-
-	putchar ('\n');
-
-	return (0);
-}tdio.h>
- /**
-  * main - main function
-* Return: always 0.
-*/
-int main(void)
-{
-char low;
-for (low = '0'; low <= '9'; low++)
-putchar(low);
-putchar ('\n');
+int x;
+for (x = '0'; x <= '9';)
+printf("%d\n", x);
 return (0);
-}~
+}
