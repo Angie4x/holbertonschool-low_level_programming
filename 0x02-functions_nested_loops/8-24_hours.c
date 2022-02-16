10822d4f7656b8209376896aa793c7335a39e83f
Angie4x <angiel.aldanac@outlook.com>
2022-02-15 19:09:18 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ * Return:
+ */
+
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar ('0' + (hours / 10));
+			_putchar ('0' + (hours % 10));
+			_putchar (':');
+			_putchar ('0' + (minutes / 10));
+			_putchar ('0' + (minutes % 10));
+			_putchar ('\n');
+		}
+	}
+}
