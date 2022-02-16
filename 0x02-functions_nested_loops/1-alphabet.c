04aab347c1f5b6dd8b45f14223e6720017a6ac9d
Angie4x <angiel.aldanac@outlook.com>
2022-02-15 16:02:56 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - imprime el alfabeto en minuscula*/
+
+void print_alphabet(void)
+{
+  char minusculas;
+
+	for (minusculas = 'a'; minusculas <= 'z'; minusculas++)
+	  {
+	  _putchar(minusculas);
+	  }
+	_putchar('\n');
+	  	
+}
