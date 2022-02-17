90727fe2960ba7018ada583527c89a15ba2a8484
Angie4x <angiel.aldanac@outlook.com>
2022-02-17 14:22:12 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isdigit - check lower c
+*@c:Variable
+*Return: 1 if c is lower 0 if not
+*/
+int _isdigit(int c)
+{
+int i;
+
+if ((c >= 48) && (c <= 58))
+return (1);
+else
+return (0);
+}
