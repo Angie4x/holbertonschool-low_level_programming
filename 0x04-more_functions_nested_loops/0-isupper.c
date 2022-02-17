e330ab052a9f48db3635d8b0bb065008cffc417c
Angie4x <angiel.aldanac@outlook.com>
2022-02-17 13:41:59 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _islower - check lower c
+*@c:Variable
+* Return: 1 if c is lower 0 if not
+*/
+int _isupper(int c)(int c)
+{
+
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
